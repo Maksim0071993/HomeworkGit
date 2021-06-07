a008429eca466a9c2a087321031ec58119b9225d
Maksim Loginov <maksim08@list.ru>
2021-06-07 17:36:49 +0300

    added first code

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+            
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            List<double> res = new List<double>();
+            for (double i = num1; i <= num2; i = i + 0.1)
+            {
+                double result = Math.Sin((i + 2) * (i + 2));
+               
+                int index = res.FindIndex(item => item == result);
+                if (index == -1)
+                {
+                    res.Add(result);
+                    Console.WriteLine($"i = {i} : sin = {result}");
+                }
+                              
+            }
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Вы ввели неверный символ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
